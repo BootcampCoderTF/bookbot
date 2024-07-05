c6971c0e3f13520c73ae971d1df2dd7b51489086
BootcampCoderTF <tmfararira@gmail.com>
2024-07-05 19:52:08 +0100

    Tidied main.py code to look cleaner

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"There are {num_words} words in ths text!")
 
-        words = file_contents.split()
-        word_count = len(words)
-        print("")
-        print(f"There are {word_count} words in ths text!")
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
 main()
